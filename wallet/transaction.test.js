17293cd5f5486999c8f1ed6a84e815c5bcc9014e
MayureshKhemnar <mayuresh.tas@gmail.com>
2022-12-23 16:17:24 +0530

    miner added

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,5 +1,6 @@
 const Transaction = require("./transaction");
 const Wallet = require("./index");
+const { MINING_REWARD } = require("../config");
 
 describe('Transaction', () => {
 
@@ -64,6 +65,18 @@ describe('Transaction', () => {
         it(`outputs an amount for the next recipient`, () => {
             expect(transaction.outputs.find(output => output.address === nextRecipent).amount)
                 .toEqual(nextAmount);
+        });
+    });
+
+    describe('creating a reward transaction', () => {
+        
+        beforeEach(() => {
+            transaction = Transaction.rewardTransaction(wallet,Wallet.blockchainWallet());
+        });
+
+        it(`reward for the miner's wallet`, () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+                .toEqual(MINING_REWARD);
         });
     });
 });
